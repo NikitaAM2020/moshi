e3ee8571a96e4ddf74a285382fc90013e43f6667
NikitaAM2020 <nikita.zhuravel24@gmail.com>
2023-04-29 19:00:32 +0300

    update lab4

--- lab4/main.py
+++ lab4/main.py
@@ -1,92 +1,111 @@
 import random
-import math
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-# Генерація випадкових даних для карти маршрутів
-n_cities = random.randint(25, 35)
-cities = [i for i in range(1, n_cities + 1)]
-distances = {}
-for i in range(1, n_cities + 1):
-    for j in range(i + 1, n_cities + 1):
-        distances[(i, j)] = distances[(j, i)] = random.randint(10, 100)
 
-# Збереження даних у файл
-with open("route_map.txt", "w") as f:
-    f.write(f"{n_cities}\n")
-    for i, city in enumerate(cities):
-        f.write(f"{i + 1} 0 0\n")
-    for (i, j), distance in distances.items():
-        f.write(f"{i} {j} {distance}\n")
+class AntColonyOptimizer:
+    def __init__(self, distances, num_ants, num_iterations, alpha=1.0, beta=2.0, evaporation_rate=0.5, Q=100):
+        self.distances = distances
+        self.num_ants = num_ants
+        self.num_iterations = num_iterations
+        self.alpha = alpha
+        self.beta = beta
+        self.evaporation_rate = evaporation_rate
+        self.Q = Q
+        self.pheromone = np.ones_like(distances) / len(distances)
+        self.best_tour = None
+        self.best_distance = float('inf')
 
-# Зчитування даних про карту маршрутів з файлу
-with open("route_map.txt", "r") as f:
-    n_cities = int(f.readline().strip())
-    cities = [tuple(map(int, f.readline().strip().split())) for _ in range(n_cities)]
-    distances = {(int(i), int(j)): int(d) for i, j, d in [line.strip().split() for line in f]}
+    def is_edge_in_tour(self, node1, node2, tour):
+        return ((node1 in tour) and (node2 in tour)) and (
+                np.abs(np.argwhere(np.array(tour) == node1) - np.argwhere(np.array(tour) == node2)) == 1)
+
+    def calculate_probabilities(self, current_node, unvisited_nodes):
+        pheromone = self.pheromone[current_node, unvisited_nodes]
+        distances = self.distances[current_node, unvisited_nodes]
+        probabilities = (pheromone ** self.alpha) * ((1.0 / distances) ** self.beta)
+        probabilities = probabilities / np.sum(probabilities)
+        return probabilities
+
+    def run(self):
+        for i in range(self.num_iterations):
+            ants = [Ant(random.randint(0, len(self.distances) - 1), self) for j in range(self.num_ants)]
+            for ant in ants:
+                ant.run()
+                if ant.distance < self.best_distance:
+                    self.best_distance = ant.distance
+                    self.best_tour = ant.tour
+            self.pheromone *= self.evaporation_rate
+            for ant in ants:
+                ant.update_pheromone(self.evaporation_rate, self.Q)
+
+    def get_best_tour(self):
+        return self.best_tour, self.best_distance
 
 
 class Ant:
-    def __init__(self, start_city_coords):
-        self.visited_cities = [cities.index(start_city_coords) + 1]
-        self.distance_travelled = 0
-
-    def select_next_city(self, pheromone, alpha, beta):
-        current_city = self.visited_cities[-1]
-        unvisited_cities = set(range(1, len(cities) + 1)) - set(self.visited_cities)
-        probabilities = []
-        for city_id in unvisited_cities:
-            city_coords = cities[city_id - 1]
-            pheromone_level = pheromone.get((current_city, city_id), 0.1)
-            visibility = distances.get(city_coords, 0.1)
-            probability = math.pow(pheromone_level, alpha) * math.pow(1 / visibility, beta)
-            probabilities.append((city_id, probability))
-        total_probability = sum(prob for _, prob in probabilities)
-        probabilities = [(city_id, prob / total_probability) for city_id, prob in probabilities]
-        selected_city_id = random.choices(population=[city_id for city_id, _ in probabilities],
-                                          weights=[prob for _, prob in probabilities])[0]
-        self.visited_cities.append(selected_city_id)
-        selected_city_coords = cities[selected_city_id - 1]
-        self.distance_travelled += distances.get((current_city, selected_city_coords), 0)
-
-    def path(self):
-        return list(zip(self.visited_cities, self.visited_cities[1:])) + [
-            (self.visited_cities[-1], self.visited_cities[0])]
-
-    def __str__(self):
-        return f"{self.visited_cities}, distance = {self.distance_travelled}"
-
-
-class AntColony:
-    def __init__(self, n_ants, n_iterations, evaporation_rate, alpha, beta):
-        self.n_ants = n_ants
-        self.n_iterations = n_iterations
-        self.evaporation_rate = evaporation_rate
-        self.alpha = alpha
-        self.beta = beta
-        self.pheromone = {(i, j): 1 for i in cities for j in cities if i != j}
-
-    def update_pheromone(self, ants):
-        for i, j in self.pheromone.keys():
-            evaporation = (1 - self.evaporation_rate) * self.pheromone[(i, j)]
-            self.pheromone[(i, j)] = evaporation
-            for ant in ants:
-                if (i, j) in ant.path():
-                    self.pheromone[(i, j)] += ant.pheromone_delta(i, j, self.alpha)
+    def __init__(self, start_node, optimizer):
+        self.optimizer = optimizer
+        self.tour = [start_node]
+        self.unvisited_nodes = list(range(len(optimizer.distances)))
+        self.unvisited_nodes.remove(start_node)
+        self.distance = 0.0
 
     def run(self):
-        best_ant = None
-        for iteration in range(self.n_iterations):
-            ants = [Ant(random.choice(cities)) for _ in range(self.n_ants)]
-            for ant in ants:
-                for _ in range(len(cities) - 1):
-                    ant.select_next_city(self.pheromone, self.alpha, self.beta)
-                ant.distance_travelled += distances.get((ant.visited_cities[-1], ant.visited_cities[0]), 0)
-                if best_ant is None or ant.distance_travelled < best_ant.distance_travelled:
-                    best_ant = ant
-            self.update_pheromone(ants)
-        return best_ant
+        while self.unvisited_nodes:
+            probabilities = self.optimizer.calculate_probabilities(self.tour[-1], self.unvisited_nodes)
+            next_node = np.random.choice(self.unvisited_nodes, p=probabilities)
+            self.tour.append(next_node)
+            self.unvisited_nodes.remove(next_node)
+            self.distance += self.optimizer.distances[self.tour[-2], self.tour[-1]]
+        self.distance += self.optimizer.distances[self.tour[-1], self.tour[0]]
+
+    def update_pheromone(self, evaporation_rate, Q):
+        for i in range(1, len(self.tour)):
+            node1, node2 = self.tour[i - 1], self.tour[i]
+            self.optimizer.pheromone[node1, node2] += (Q / self.distance)
+            self.optimizer.pheromone[node2, node1] += (Q / self.distance)
 
 
-print("Кількість міст = ", n_cities)
-colony = AntColony(n_ants=50, n_iterations=100, evaporation_rate=0.1, alpha=1, beta=3)
-best_ant = colony.run()
-print("Найкоротший шлях: ", best_ant.visited_cities)
+# generate distances matrix
+N = random.randint(25, 35)
+distances = np.zeros((N, N))
+for i in range(N):
+    for j in range(i + 1, N):
+        distances[i][j] = distances[j][i] = random.randint(10, 100)
+
+
+# запис даних щодо згенерованої карти у файл
+with open('map.txt', 'w') as f:
+    f.write(f'{N}\n')
+    for row in distances:
+        f.write(' '.join(map(str, row)) + '\n')
+# print(distances)
+
+min_distance = np.min(distances)
+max_distance = np.max(distances)
+
+print("Minimum distance:", min_distance)
+print("Maximum distance:", max_distance)
+
+avg_distance = np.mean(distances)
+
+print("Average distance:", avg_distance)
+min_indices = np.unravel_index(np.argmin(distances), distances.shape)
+print("Index of closest nodes:", min_indices)
+
+max_indices = np.unravel_index(np.argmax(distances), distances.shape)
+print("Index of farthest nodes:", max_indices)
+
+sns.heatmap(distances, cmap="YlGnBu", annot=True, fmt=".0f")
+plt.title("Distances Matrix")
+plt.show()
+
+aco = AntColonyOptimizer(distances, num_ants=10, num_iterations=100, alpha=1.0, beta=2.0, evaporation_rate=0.5, Q=100)
+aco.run()
+
+best_tour, best_distance = aco.get_best_tour()
+
+print("Best tour:", best_tour)
+print("Best distance:", best_distance)
