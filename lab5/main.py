766c3035582c6150c821be457a93ae5cdd386278
NikitaAM2020 <nikita.zhuravel24@gmail.com>
2023-05-18 18:53:22 +0300

    update lab 5

--- lab5/main.py
+++ lab5/main.py
@@ -1,71 +1,37 @@
 import random
 
-# Параметри задачі
-NUM_LESSONS = 25
-NUM_TEACHERS = 4
-NUM_CLASSES = 3
-MAX_LESSONS_PER_DAY = 5
-NUM_SPECIAL_ROOMS = 1
+# Константи та параметри
+DAYS_OF_WEEK = ["Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця"]
+NUM_LESSONS = 25  # Кількість уроків на тиждень
+MAX_LESSONS_PER_DAY = 5  # Максимальна кількість уроків у день
 
-# Задання назв предметів, вчителів та класів
-subjects = {
-    (1, 1): "Математика",
-    (2, 1): "Українська мова",
-    (3, 1): "Англійська мова",
-    (4, 1): "Музичне мистецтво",
-    (1, 2): "Інформатика",
-    (2, 2): "Я досліджую світ",
-    (3, 2): "Образотворче мистецтво",
-    (4, 2): "Фізкультура",
-    (1, 3): "Математика",
-    (2, 3): "Українська мова",
-    (3, 3): "Фізкультура",
-    (4, 3): "Інформатика",
-}
-
-teachers = {
-    (1, 1): "Василенко",
-    (2, 1): "Шевченко",
-    (3, 1): "Мельникук",
-    (4, 1): "Омельчук",
-    (1, 2): "Інші вчителі",
-    (2, 2): "Інші вчителі",
-    (3, 2): "Інші вчителі",
-    (4, 2): "Інші вчителі",
-    (1, 3): "Інші вчителі",
-    (2, 3): "Інші вчителі",
-    (3, 3): "Інші вчителі",
-    (4, 3): "Інші вчителі"
-}
-
-classes = {
-    1: "1A",
-    2: "2A",
-    3: "3A",
-    4: "Інші класи",
-    5: "Інші класи",
-    6: "Інші класи"
-}
-
-# Задання назв днів тижня
-days_of_week = ["Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця"]
+# Предмети
+subjects = ['Математика', 'Українська мова', 'Англійська мова', 'Фізкультура', 'Музика', 'Малювання',
+            'Трудове навчання', 'Природознавство', 'Географія', 'Історія', 'Математика', 'Українська мова',
+            'Англійська мова', 'Фізкультура', 'Музика', 'Малювання',
+            'Трудове навчання', 'Природознавство', 'Географія', 'Історія', 'Математика', 'Українська мова',
+            'Англійська мова', 'Фізкультура', 'Музика']
 
 
-# Генерація початкової популяції
+# Функція генерації початкової популяції
 def generate_schedule():
     schedule = []
     for _ in range(NUM_LESSONS):
-        teacher = random.randint(1, NUM_TEACHERS)
-        time_slot = random.randint(1, MAX_LESSONS_PER_DAY)
-        schedule.append((teacher, time_slot))
+        day_of_week = random.choice(DAYS_OF_WEEK)
+        schedule.append(day_of_week)
     return schedule
 
 
-# Функція придатності
+# Функція обчислення функції придатності
 def fitness(schedule):
-    # Розрахунок функції придатності (наприклад, кількість "вікон")
+    # Розрахунок функції придатності (наприклад, кількість уроків у день та "вікон")
     fitness_score = 0
-    # Додаткові перевірки можуть бути виконані для оцінки якості розкладу
+    for day in DAYS_OF_WEEK:
+        lessons = schedule.count(day)
+        if lessons > MAX_LESSONS_PER_DAY:
+            fitness_score -= (lessons - MAX_LESSONS_PER_DAY)
+        if lessons < MAX_LESSONS_PER_DAY:
+            fitness_score -= (MAX_LESSONS_PER_DAY - lessons)
     return fitness_score
 
 
@@ -80,79 +46,59 @@ def crossover(parent1, parent2):
 
 # Оператор мутації
 def mutate(schedule):
-    # Реалізація оператора мутації (напририкладу)
+    # Реалізація оператора мутації (наприкладу, заміна дня)
     mutated_schedule = schedule[:]
     gene_index = random.randint(0, NUM_LESSONS - 1)
-    gene = random.randint(0, NUM_LESSONS - 1)
-    mutated_schedule[gene] = (random.randint(1, NUM_TEACHERS), random.randint(1, MAX_LESSONS_PER_DAY))
+    mutated_schedule[gene_index] = random.choice(DAYS_OF_WEEK)
     return mutated_schedule
 
 
 def selection(population):
-    # Реалізація функції вибору найкращих особин (наприклад, турнірний відбір)
-    selected = random.sample(population, 2)
-    fitness_scores = [fitness(individual) for individual in selected]
-    best_individual = selected[fitness_scores.index(max(fitness_scores))]
-    return best_individual
+    # Реалізація оператора відбору (наприклад, турнірний відбір)
+    tournament_size = 2
+    selected_population = []
+    while len(selected_population) < len(population):
+        tournament = random.sample(population, tournament_size)
+        best_schedule = max(tournament, key=fitness)
+        selected_population.append(best_schedule)
+    return selected_population
 
 
 def genetic_algorithm():
     # Генерація початкової популяції
-    population = {class_name: [generate_schedule() for _ in range(NUM_LESSONS)] for class_name in classes.values()}
+    population_size = 10
+    population = [generate_schedule() for _ in range(population_size)]
 
-    # Цикл оптимізації
-    for _ in range(100):
-        # Створення нового покоління
-        new_population = {class_name: [] for class_name in classes.values()}
-        for class_name in classes.values():
-            # Вибір найкращих особин
-            parent1 = selection(population[class_name])
-            parent2 = selection(population[class_name])
+    # Еволюція популяції
+    num_generations = 50
+    for generation in range(num_generations):
+        # print(f"Покоління {generation + 1}")
 
-            # Схрещування
+        # Вибір найкращих особин
+        selected_population = selection(population)
+
+        # Схрещування
+        offspring = []
+        while len(offspring) < population_size:
+            parent1, parent2 = random.sample(selected_population, 2)
             child1, child2 = crossover(parent1, parent2)
+            offspring.append(child1)
+            offspring.append(child2)
 
-            # Мутація
-            mutated_child1 = mutate(child1)
-            mutated_child2 = mutate(child2)
+        # Мутація
+        mutated_offspring = [mutate(schedule) for schedule in offspring]
 
-            # Додавання дитини в нову популяцію
-            new_population[class_name].append(mutated_child1)
-            new_population[class_name].append(mutated_child2)
+        # Оновлення популяції
+        population = mutated_offspring
 
-        # Заміна попередньої популяції новою
-        population = new_population
-
-    # Об'єднання розкладів для кожного класу
-    best_schedule = []
-    for class_name in classes.values():
-        best_schedule.extend(population[class_name])
-
-    return best_schedule
+    # Виведення результатів
+    for i, schedule in enumerate(population, 1):
+        print(f"Розклад для класу {i}:")
+        for day in DAYS_OF_WEEK:
+            lessons = [subjects[index] for index, lesson in enumerate(schedule) if lesson == day]
+            print(f"{day}: {', '.join(lessons)}")
+        print()
 
 
-best_schedule = genetic_algorithm()
-
-# Створення словника для зберігання уроків за класами
-class_schedule = {class_name: [] for class_name in classes.values()}
-
-# Додавання уроків до відповідних класів
-for lesson in best_schedule:
-    teacher = teachers[lesson[0]]
-    time_slot = lesson[1]
-    day_of_week = random.choice(days_of_week)  # Випадковий день тижня
-    lesson_class = classes[lesson[1]]
-
-    subject = subjects[(lesson[0], lesson[2])]
-    class_schedule[lesson_class].append((subject, teacher, day_of_week, time_slot))
-
-# Виведення розкладу за класами
-for class_name, lessons in class_schedule.items():
-    print(f"Клас: {class_name}")
-    for lesson in lessons:
-        subject = lesson[0]
-        teacher = lesson[1]
-        day_of_week = lesson[2]
-        time_slot = lesson[3]
-        print(f"Предмет: {subject}, Вчитель: {teacher}, День тижня: {day_of_week}, Час: {time_slot}")
-    print()
+# Запуск генетичного алгоритму
+genetic_algorithm()
