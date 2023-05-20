e94eb0b3ec756de2824a86d767dd9a3c36d4afd7
NikitaAM2020 <nikita.zhuravel24@gmail.com>
2023-05-20 16:07:44 +0300

    add new lab 6

--- lab6/main.py
+++ lab6/main.py
@@ -1,128 +1,111 @@
-import random
+from tkinter import *
 
 
-class TreeNode:
-    def __init__(self, state):
-        self.state = state
-        self.children = []
+class TicTackToe:
+    def __init__(self):
+        self.board = [' '] * 9
+        self.player = 'X'
+        self.computer = 'O'
 
-    def add_child(self, node):
-        self.children.append(node)
+        self.root = Tk()
+        self.root.title("lab6")
+        self.root.geometry('285x320')
 
+        self.buttons = []
 
-def build_tree(state, player):
-    node = TreeNode(state)
-    winner = check_winner(state)
+        for i in range(9):
+            self.buttons.append(Button(self.root, text=' ', width=5, height=2, font=('Helvetica', 22),
+                                       command=lambda I=i: self.move(I)))
+            self.buttons[i].grid(row=i // 3, column=i % 3)
 
-    if winner is not None:
-        return node
+        self.root.mainloop()
 
-    for i in range(3):
-        for j in range(3):
-            if state[i][j] == "-":
-                new_state = [row.copy() for row in state]
-                new_state[i][j] = player
-                node.add_child(build_tree(new_state, "O" if player == "X" else "X"))
-
-    return node
-
-
-def print_board(state):
-    for row in state:
-        print(" ".join(row))
-
-
-def check_winner(state):
-    lines = state + [[state[i][j] for i in range(3)] for j in range(3)] + [[state[i][i] for i in range(3)]] + [
-        [state[i][2 - i] for i in range(3)]]
-
-    for line in lines:
-        if line == ["X", "X", "X"]:
-            return "X"
-        elif line == ["O", "O", "O"]:
-            return "O"
-
-    return None
-
-
-def get_human_move(state):
-    while True:
-        move = input("Enter your move (row column): ")
-        parts = move.split()
-        if len(parts) == 2 and parts[0].isdigit() and parts[1].isdigit():
-            row = int(parts[0])
-            col = int(parts[1])
-            if 0 <= row < 3 and 0 <= col < 3 and state[row][col] == "-":
-                return row, col
-        print("Invalid move. Try again.")
-
-
-def get_computer_move(tree):
-    best_score = float("-inf")
-    best_move = None
-
-    for child in tree.children:
-        score = minimax(child, False)
-        if score > best_score:
-            best_score = score
-            best_move = child.state
-
-    for i in range(3):
-        for j in range(3):
-            if tree.state[i][j] != best_move[i][j]:
-                return i, j
-
-
-def minimax(node, maximizing_player):
-    winner = check_winner(node.state)
-
-    if winner is not None:
-        return 1 if winner == "X" else -1
-
-    if maximizing_player:
-        max_eval = float("-inf")
-        for child in node.children:
-            eval = minimax(child, False)
-            max_eval = max(max_eval, eval)
-        return max_eval
-    else:
-        min_eval = float("inf")
-        for child in node.children:
-            eval = minimax(child, True)
-            min_eval = min(min_eval, eval)
-    return min_eval
-
-
-def play_game():
-    state = [["-" for _ in range(3)] for _ in range(3)]
-    current_player = "X"
-    tree = build_tree(state, current_player)
-    while True:
-        print_board(state)
-
-        if current_player == "X":
-            row, col = get_human_move(state)
-            state[row][col] = current_player
-        else:
-            row, col = get_computer_move(tree)
-            state[row][col] = current_player
-
-        winner = check_winner(state)
-
-        if winner is not None:
-            print_board(state)
-            if winner == "X":
-                print("You win!")
+    def move(self, index):
+        if self.board[index] == ' ':
+            self.board[index] = self.player
+            self.buttons[index].config(text=self.player, state=DISABLED)
+            if self.check_winner(self.board, self.player):
+                self.game_over("Вітаємо, ви виграли!")
+            elif self.check_tie(self.board):
+                self.game_over("Нічія!")
             else:
-                print("Computer wins!")
-            break
+                self.computer_move()
 
-        if all(state[i][j] != "-" for i in range(3) for j in range(3)):
-            print_board(state)
-            print("It's a tie!")
-            break
+    def computer_move(self):
+        best_score = -float('inf')
+        best_move = None
+        for i in range(9):
+            if self.board[i] == ' ':
+                self.board[i] = self.computer
+                score = self.minimax(self.board, False)
+                self.board[i] = ' '
+                if score > best_score:
+                    best_score = score
+                    best_move = i
+        self.board[best_move] = self.computer
+        self.buttons[best_move].config(text=self.computer, state=DISABLED)
+        if self.check_winner(self.board, self.computer):
+            self.game_over("Комп'ютер виграв!")
+        elif self.check_tie(self.board):
+            self.game_over("Нічія!")
 
-        current_player = "O" if current_player == "X" else "X"
+    def minimax(self, board, maximizing_player):
+        if self.check_winner(board, self.computer):
+            return 1
+        elif self.check_winner(board, self.player):
+            return -1
+        elif self.check_tie(board):
+            return 0
+        if maximizing_player:
+            best_score = -float('inf')  # Change this line
+            for i in range(9):
+                if board[i] == ' ':
+                    board[i] = self.player
+                    score = self.minimax(board, False)
+                    board[i] = ' '
+                    best_score = max(score, best_score)  # Change this line
+            return best_score
+        else:
+            best_score = float('inf')
+            for i in range(9):
+                if board[i] == ' ':
+                    board[i] = self.computer
+                    score = self.minimax(board, True)
+                    board[i] = ' '
+                    best_score = min(score, best_score)
+            return best_score
+
+    def check_winner(self, board, player):
+        win = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
+        for i in win:
+            if board[i[0]] == board[i[1]] == board[i[2]] == player:
+                return True
+        return False
+
+    def check_tie(self, board):
+        return all(x != ' ' for x in board)
+
+    def game_over(self, message):
+        for button in self.buttons:
+            button.config(state=DISABLED)
+        label = Label(self.root, text=message)  # Change this line
+        label.grid(row=3, column=0, columnspan=3)
+        play_again = Button(self.root, text="Грати знову", command=self.restart)
+        play_again.grid(row=4, column=0, columnspan=3)
+
+    def restart(self):
+        self.board = [' '] * 9
+        self.player = 'X'
+        self.computer = 'O'
+        for i in range(9):
+            self.buttons[i].config(text=' ', state=NORMAL)
+        for widget in self.root.grid_slaves():
+            if isinstance(widget, Label):
+                widget.destroy()
+        label = Label(self.root, text='')
+        label.grid(row=3, column=0, columnspan=3)
+        play_again = Button(self.root, text="Грати знову!", command=self.restart)
+        play_again.grid(row=4, column=0, columnspan=3)
 
 
-play_game()
+game = TicTackToe()
